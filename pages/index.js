2b8ab70d40bb0d6b8ed3ff7999c19ab816dfeed3
alisonqiu <mythpuzzle@gmail.com>
2022-01-25 16:11:46 -0600

    image issue

--- pages/index.js
+++ pages/index.js
@@ -6,10 +6,14 @@ import Link from 'next/link'
 import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns'
+import Col from 'react-bootstrap/Col'
+import Row from 'react-bootstrap/Row'
+import Container from 'react-bootstrap/Container'
 
 import arrow from '../images/Arrow.png'
 import orcas from '../images/homepage.png'
 import vector from '../images/vector.png'
+import map from '../images/homepagemap.png'
 import indexStyles from '../styles/Index.module.css'
 
 export const index = () => {
@@ -69,37 +73,49 @@ export const index = () => {
           </p>
 
           <div className={indexStyles.card}>
-            <CardColumns>
-              <Card>
-                <Card.Img
-                  variant="top"
-                  src="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=660%2C725&ssl=1"
-                />
-                <Card.Body></Card.Body>
-              </Card>
-              <Card>
-                <Card.Body>
-                  <Card.Title> Hydrophone Location</Card.Title>
-                  <br></br>
-                  <Card.Text>
-                    Orcasound is a cooperative hydrophne network and an
-                    open-source software & hardware project.
+          <Container fluid>
+            <Row className={indexStyles.card} >
+              <Col>
+                <Card className="border-0 text-center">
+                  <Card.Img
+                    variant="top"
+                    src="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=660%2C725&ssl=1"
+                  />
+                  <Card.Body>
+                    <Card.Text className={indexStyles.cardText2}>Current Orcasound hydrophone locations</Card.Text>
+                  </Card.Body>
+                </Card>
+              </Col>
+              <Col>
+                <Card className="border-0">
+                  <Card.Body>
+                    <Card.Title className={indexStyles.cardTitle}> Hydrophone Location</Card.Title>
                     <br></br>
-                    <br></br>
-                    We Welcome your participation. If you'd like to host a
-                    hydrophone, do research or incorporate Orcasound into the
-                    educational or research efforts of your organization , you
-                    can join us as a member of the network.
-                    <br></br>
-                  </Card.Text>
-                  <div className={indexStyles.button}>
-                    <br></br>
-                    <Button variant="outline-primary"> LEARN</Button>{' '}
-                    <Button variant="outline-primary"> GET INVOLVED!</Button>{' '}
-                  </div>
-                </Card.Body>
-              </Card>
-            </CardColumns>
+                    <Card.Text className={indexStyles.cardText}>
+                      Orcasound is a cooperative hydrophne network and an
+                      open-source software & hardware project.
+                    </Card.Text>
+                  </Card.Body>
+                </Card>
+                <Card className="border-0">
+                  <Card.Body>
+                    <Card.Text className={indexStyles.cardText}>
+                      We Welcome your participation. If you'd like to host a
+                      hydrophone, do research or incorporate Orcasound into the
+                      educational or research efforts of your organization , you
+                      can join us as a member of the network.
+                      <br></br>
+                    </Card.Text>
+                    <div className={indexStyles.button}>
+                      <br></br>
+                      <Button className={indexStyles.cardButton}> LEARN</Button>{' '}
+                      <Button className={indexStyles.cardButton}> GET INVOLVED!</Button>{' '}
+                    </div>
+                  </Card.Body>
+                </Card>
+              </Col>
+            </Row>
+            </Container>
           </div>
         </body>
       </div>
