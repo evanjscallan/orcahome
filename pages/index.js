ff34d4647763bdb807c1deb5235f5bb5448195b7
alisonqiu <mythpuzzle@gmail.com>
2022-01-26 13:18:43 -0600

    first round

--- pages/index.js
+++ pages/index.js
@@ -7,13 +7,13 @@ import Button from 'react-bootstrap/Button'
 import Card from 'react-bootstrap/Card'
 import CardColumns from 'react-bootstrap/CardColumns'
 import Col from 'react-bootstrap/Col'
-import Row from 'react-bootstrap/Row'
 import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
 
 import arrow from '../images/Arrow.png'
 import orcas from '../images/homepage.png'
-import vector from '../images/vector.png'
 import map from '../images/homepagemap.png'
+import vector from '../images/vector.png'
 import indexStyles from '../styles/Index.module.css'
 
 export const index = () => {
@@ -72,51 +72,60 @@ export const index = () => {
             whales or learn more about marine bioacoustics.
           </p>
 
-          <div className={indexStyles.card}>
-          <Container fluid>
-            <Row className={indexStyles.card} >
-              <Col>
-                <Card className="border-0 text-center">
-                  <Card.Img
-                    variant="top"
-                    src="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=660%2C725&ssl=1"
-                  />
-                  <Card.Body>
-                    <Card.Text className={indexStyles.cardText2}>Current Orcasound hydrophone locations</Card.Text>
-                  </Card.Body>
-                </Card>
-              </Col>
-              <Col>
-                <Card className="border-0">
-                  <Card.Body>
-                    <Card.Title className={indexStyles.cardTitle}> Hydrophone Location</Card.Title>
+          <Row className={indexStyles.card}>
+            <Col md={{ order: 1 }} xs={{ order: 2 }} className={indexStyles.col}>
+              <Card className="border-0 text-center">
+                <Card.Img
+                  variant="top"
+                  src="https://i2.wp.com/www.orcasound.net/wp2017/wp-content/uploads/2020/10/Screen-Shot-2020-10-20-at-1.40.28-PM.png?resize=750%2C725&ssl=1"
+                />
+                <Card.Body>
+                  <Card.Text className={indexStyles.cardText2}>
+                    Current Orcasound hydrophone locations
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={{ order: 2 }} xs={{ order: 1 }} className={indexStyles.col}>
+              <Card className="border-0">
+                <Card.Body>
+                  <Card.Title className={indexStyles.cardTitle}>
+                    {' '}
+                    Hydrophone Location
+                  </Card.Title>
+                  <br></br>
+                  <Card.Text className={indexStyles.cardText}>
+                    Orcasound is a cooperative hydrophne network and an
+                    open-source software & hardware project.
+                  </Card.Text>
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col md={{ order: 3 }} xs={{ order: 3 }} className={indexStyles.col}>
+              <Card className="border-0">
+                <Card.Body>
+                  <Card.Text className={indexStyles.cardText}>
+                    We Welcome your participation. If you'd like to host a
+                    hydrophone, do research or incorporate Orcasound into the
+                    educational or research efforts of your organization , you
+                    can join us as a member of the network.
                     <br></br>
-                    <Card.Text className={indexStyles.cardText}>
-                      Orcasound is a cooperative hydrophne network and an
-                      open-source software & hardware project.
-                    </Card.Text>
-                  </Card.Body>
-                </Card>
-                <Card className="border-0">
-                  <Card.Body>
-                    <Card.Text className={indexStyles.cardText}>
-                      We Welcome your participation. If you'd like to host a
-                      hydrophone, do research or incorporate Orcasound into the
-                      educational or research efforts of your organization , you
-                      can join us as a member of the network.
-                      <br></br>
-                    </Card.Text>
-                    <div className={indexStyles.button}>
-                      <br></br>
-                      <Button className={indexStyles.cardButton}> LEARN</Button>{' '}
-                      <Button className={indexStyles.cardButton}> GET INVOLVED!</Button>{' '}
-                    </div>
-                  </Card.Body>
-                </Card>
-              </Col>
-            </Row>
-            </Container>
-          </div>
+                  </Card.Text>
+                  <div className={indexStyles.button}>
+                    <br></br>
+                    <Button className={indexStyles.cardButton}>
+                      {' '}
+                      LEARN
+                    </Button>{' '}
+                    <Button className={indexStyles.cardButton}>
+                      {' '}
+                      GET INVOLVED!
+                    </Button>{' '}
+                  </div>
+                </Card.Body>
+              </Card>
+            </Col>
+          </Row>
         </body>
       </div>
     </>
